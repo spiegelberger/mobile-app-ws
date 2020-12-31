cb3937e18ed1f7ecf9564bcb3885cc4e95550647
Zsolt Spiegelberger <zsoltspiegelberger@gmail.com>
2020-12-31 05:48:29 +0100

    Address Response class added User Rest class modified accordingly

--- src/main/java/com/spiegelberger/app/ws/ui/model/response/UserRest.java
+++ src/main/java/com/spiegelberger/app/ws/ui/model/response/UserRest.java
@@ -1,11 +1,15 @@
 package com.spiegelberger.app.ws.ui.model.response;
 
+import java.util.List;
+
 public class UserRest {
 
 	private String userId;
 	private String firstName;
 	private String lastName;
 	private String email;
+	private List<AddressRest>addresses;
+	
 	
 	public String getUserId() {
 		return userId;
@@ -30,6 +34,12 @@ public class UserRest {
 	}
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	public List<AddressRest> getAddresses() {
+		return addresses;
+	}
+	public void setAddresses(List<AddressRest> addresses) {
+		this.addresses = addresses;
 	}
 	
 	
